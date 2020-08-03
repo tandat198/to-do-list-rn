e9b8cf6b773819e841c7bfcd2b6680a924132828
Dat Ng <tandat198@gmail.com>
2020-08-03 10:23:45 +0700

    update

--- App.js
+++ App.js
@@ -5,18 +5,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./screens/Home";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+// import store from "./redux/store";
 
 const Stack = createStackNavigator();
 
 export default function App() {
-    return (
-        <Provider store={store}>
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen screen='Home' component={Home} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </Provider>
-    );
+  return (
+    // <Provider store={store}>
+
+    // </Provider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,70 +1,92 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import { Text, Button } from "react-native-elements";
+import { StyleSheet, View, FlatList } from "react-native";
+import { Text, Button, Overlay, Input, CheckBox } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function Home() {
-    const [visibleCreateOverlay, setVisibleCreateOverlay] = useState(false);
+  const [visibleCreateOverlay, setVisibleCreateOverlay] = useState(false);
 
-    const tasks = [
-        {
-            id: 1,
-            name: "Do something",
-            isDone: false,
-        },
-        {
-            id: 2,
-            name: "Do Something else",
-            isDone: true,
-        },
-    ];
+  const tasks = [
+    {
+      id: 1,
+      name: "Do something",
+      isDone: false,
+    },
+    {
+      id: 2,
+      name: "Do Something else",
+      isDone: true,
+    },
+  ];
 
-    const toggleOverlayCreate = () => {
-        setVisibleCreateOverlay(!visibleCreateOverlay);
-    };
+  const toggleOverlayCreate = () => {
+    setVisibleCreateOverlay(!visibleCreateOverlay);
+  };
 
-    const TaskItem = ({ task }) => (
-        <View style={styles.listItem}>
-            <CheckBox style={styles.checkBox} checked={task.isDone} />
-            <Text h4>{task.name}</Text>
-        </View>
-    );
+  const TaskItem = ({ item }) => (
+    <View style={styles.listItem}>
+      <CheckBox style={styles.checkBox} checked={item.isDone} />
+      <Text h4>{item.name}</Text>
+    </View>
+  );
 
-    return (
-        <View style={style.container}>
-            <View style={style.titleContainer}>
-                <Text h2>My Tasks</Text>
-            </View>
-            <View>
-                <Button
-                    onPress={toggleOverlayCreate}
-                    icon={<Icon name='plus' size={15} color='white' />}
-                    title='Add Task'
-                />
-            </View>
-            <View>
-                <FlatList keyExtractor={(item) => item.id.toString()} data={tasks} renderItem={TaskItem} />
-            </View>
-            <Overlay isVisible={visibleCreateOverlay} onBackdropPress={toggleOverlayCreate}>
-                <Input placeholder='Your Task' />
-                <Button onPress={toggleOverlayCreate} icon={<Icon name='plus' size={15} color='white' />} title='Add' />
-            </Overlay>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title} h2>
+          My Tasks
+        </Text>
+      </View>
+      <View>
+        <Button
+          onPress={toggleOverlayCreate}
+          icon={<Icon name="plus" size={15} color="white" />}
+          title="Add Task"
+        />
+      </View>
+      <View style={styles.list}>
+        <FlatList
+          keyExtractor={(item) => item.id.toString()}
+          data={tasks}
+          renderItem={TaskItem}
+        />
+      </View>
+      <Overlay
+        isVisible={visibleCreateOverlay}
+        onBackdropPress={toggleOverlayCreate}
+        overlayStyle={styles.overlay}
+      >
+        <Input placeholder="Your Task" />
+        <Button
+          onPress={toggleOverlayCreate}
+          icon={<Icon name="plus" size={15} color="white" />}
+          title="Add"
+        />
+      </Overlay>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    titleContainer: {
-        textAlign: "center",
-    },
-    checkBox: {
-        marginRight: 5,
-    },
-    listItem: {
-        flexDirection: "row",
-        flexWrap: "nowrap",
-    },
+  container: {
+    flex: 1,
+    paddingTop: 10,
+  },
+  title: {
+    textAlign: "center",
+  },
+  checkBox: {
+    marginRight: 5,
+  },
+  list: {
+    marginTop: 20,
+  },
+  listItem: {
+    flexDirection: "row",
+    flexWrap: "nowrap",
+  },
+  overlay: {
+    width: "80%",
+    paddingVertical: 30,
+  },
 });
