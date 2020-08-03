405d79cbb7711b48d62972387553f6324fa92b9a
tandat198 <tandat198@gmail.com>
2020-08-03 07:54:06 +0700

    update

--- App.js
+++ App.js
@@ -4,15 +4,19 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./screens/Home";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const Stack = createStackNavigator();
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen screen='Home' component={Home} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Provider store={store}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen screen='Home' component={Home} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </Provider>
     );
 }
